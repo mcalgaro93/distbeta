1a943a8160d8e922a4dc7ca7c190cdd357cb463f
mcalgaro93 <43474662+mcalgaro93@users.noreply.github.com>
2018-09-21 17:20:20 +0200

    Create timeplots.R

---
+++ timeplots.R
@@ -0,0 +1,13 @@
+library(phyloseq)
+library(vegan)
+library(ggplot2)
+
+dist_matrix <- phyloseq::distance(phyloseq_object , method = "unifrac")
+beta <- as.matrix(dist_matrix)
+db <- distbeta.beta(beta = beta,metadata = as.data.frame(sample_data(phyloseq_object)),samplevar = "Sample",condvar = c("Treatment","Gender","Age"),timevar = "TimePoint",timediff = c("T0-T1","T1-T2","T2-T3"))
+df <- rbind(db$`T0-T1`,db$`T1-T2`,db$`T2-T3`)
+
+ggplot(df,aes(x = timepoint,y = dist,color = Sample)) + 
+  geom_point() + 
+  geom_path(aes(group = Sample),size = 1) 
+  facet_grid(Gender~Treatment) 
