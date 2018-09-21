08efe488f1376b7299e99259361a72acd1a83120
Matteo Calgaro <43474662+mcalgaro93@users.noreply.github.com>
2018-09-21 17:41:55 +0200

    Update timeplots.R

--- timeplots.R
+++ timeplots.R
@@ -9,5 +9,5 @@ df <- rbind(db$`T0-T1`,db$`T1-T2`,db$`T2-T3`)
 
 ggplot(df,aes(x = timepoint,y = dist,color = Sample)) + 
   geom_point() + 
-  geom_path(aes(group = Sample),size = 1) 
+  geom_path(aes(group = Sample),size = 1) +
   facet_grid(Gender~Treatment) 
